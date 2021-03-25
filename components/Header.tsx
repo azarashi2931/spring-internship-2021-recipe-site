71350a41acb522b40188adcf700ad0fceb20aac3
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-03-26 04:13:24 +0900

    add ogp tags

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 import NextHeader from "next/head";
 import Link from "next/link";
 import { SearchForm } from "./SearchForm";
-import { SiteTitle } from "../lib/site-data";
+import { SiteTitle, SiteDescription } from "../lib/site-data";
 
 type Props = { initialQuery: string };
 
 export const Header: NextPage<Props> = (props) => {
+  const router = useRouter();
+
   return (
     <div className="header">
       <NextHeader>
         <title>{SiteTitle}</title>
+        <meta name="description" content={SiteDescription} />
+        <meta property="og:url" content={router.asPath} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={SiteTitle} />
+        <meta property="og:description" content={SiteDescription} />
       </NextHeader>
       <Link href="/">
         <h1 className="title">{SiteTitle}</h1>
--- lib/site-data.ts
+++ lib/site-data.ts
@@ -1,1 +1,2 @@
 export const SiteTitle = "サイト名";
+export const SiteDescription = "サイト説明";
